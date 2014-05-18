15e58926359ef8f67ba3316154016f771184dec6
root <root@manage.h4g.co>
2014-05-18 17:08:55 -0400

    My first commit

---
+++ zabbix_agent.sh
@@ -0,0 +1,7 @@
+yum install http://mirror.iprimus.com.au/epel/6/x86_64/epel-release-6-8.noarch.rpm -y
+yum install zabbix20-agent -y -y
+cd /etc
+mv zabbix_agentd.conf
+wget http://url
+service zabbix-agent start
+
